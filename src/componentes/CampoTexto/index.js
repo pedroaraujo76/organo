790593c275c29cc9fcf35af113759cc9aa4048b1
pedroaraujo76 <pedrohenriquelimdf2508@gmail.com>
2023-04-12 18:08:38 -0300

    att 1

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,13 +1,20 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
-    console.log(props)
+
+    const placeholderModificada = `${props.placeholder}...` 
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value);
+    }
+
     return (
         <div className="campo-texto">
             <label>
-                {props.label} 
+                {props.label}
             </label>
-            <input placeholder={props.placeholder} />
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
